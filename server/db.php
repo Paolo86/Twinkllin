7e62ec82d2de79bd0dfe977ea308121264f916ed
Paolo86 <p.ferri1986@gmail.com>
2018-11-29 14:03:09 +0800

    Changing collection page

--- server/db.php
+++ server/db.php
@@ -11,13 +11,19 @@ while($row = getRow($result))
 
 
 
-echo '<div  class="pcollection-box" id=" ' . $row["ID"] . '" >';
+  echo '<image style="width: 200pt" class="img-rounded img-responsive" src="' . $row['Link'] . '"></image>' ;
+    
+
+
+
+/*echo '<div  class="pcollection-box" id=" ' . $row["ID"] . '" >';
 echo '<div >';
 echo '<image class="sampleImage img-rounded" src="' . $row['Link'] . '"></image>' ;
 echo '</div>';
 echo '<p class="item-details" id="title">'.  $row['Name'] . '</p>';
 echo '<p class="item-details" id ="details-item">Price: '.  $row['Price'] . '</p>';
-echo '<p class="item-details" id ="details-item">Available: '.  $row['Stock'] . '</p>';
+echo '<p class="item-details" id ="details-item">Available: '.  $row['Stock'] . '</p>';*/
+
 $stock =  $row['Stock'];
 if($stock > 0)
 {
