019f4fbac0f8175ad1f0caf5ac92ea369d0f211c
Paolo86 <p.ferri1986@gmail.com>
2018-11-29 13:37:35 +0800

    Removed unused css

--- server/db.php
+++ server/db.php
@@ -13,7 +13,7 @@ while($row = getRow($result))
 
 echo '<div  class="pcollection-box" id=" ' . $row["ID"] . '" >';
 echo '<div >';
-echo '<image class="sampleImage" src="' . $row['Link'] . '"></image>' ;
+echo '<image class="sampleImage img-rounded" src="' . $row['Link'] . '"></image>' ;
 echo '</div>';
 echo '<p class="item-details" id="title">'.  $row['Name'] . '</p>';
 echo '<p class="item-details" id ="details-item">Price: '.  $row['Price'] . '</p>';
