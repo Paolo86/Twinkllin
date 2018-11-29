60e46413ba9d722921da16bba7c8ceb22aa39196
Paolo86 <p.ferri1986@gmail.com>
2018-11-29 12:01:44 +0800

    Added spacer

--- js/navbarscroll.js
+++ js/navbarscroll.js
@@ -1,25 +1,30 @@
 window.onscroll = function()
 {
-	console.log("Doc scroll top " + document.documentElement.scrollTop);
-	var scroll = document.documentElement.scrollTop;
 	
+	var scroll = document.documentElement.scrollTop;
+
+	var sticky = document.getElementById("stickyNav");
+	var logo = document.getElementById("logoImg");
+
 	if(scroll > 50)
 	{
 		
-		var sticky = document.getElementById("stickyNav");
-		var logo = document.getElementById("logoImg");
+	
 		sticky.style.padding = "0pt";
 		logo.style.width = "0pt";
 		logo.style.width = "0%";
+		
+		
 	}
 	else
 	{
-		var sticky = document.getElementById("stickyNav");
-		var logo = document.getElementById("logoImg");
+	
 		
 		sticky.style.padding = "10pt";
 		logo.style.width = "10%";
 		logo.style.height = "10%";
 	}
 	
+	
 }
+
