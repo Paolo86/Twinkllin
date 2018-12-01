3cd5b280eebcb3a8e114c6b84bcd5d8bf5248d2a
Paolo86 <p.ferri1986@gmail.com>
2018-12-01 16:47:10 +0800

    Removed unused js, edited main.css

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-//code for login menu to fade in and out when clicked
-$(document).ready(function(){
-  var arrow = $(".arrow-up");
-  var form = $(".login-form");
-  var status = false;
-  $("#login").click(function(event){
-    event.preventDefault();
-    if(status == false){
-      arrow.fadeIn();
-      form.fadeIn();
-      status = true;
-    }else{
-      arrow.fadeOut();
-      form.fadeOut();
-      status = false;
-    }
-  })
-})
+
 
 function toHome()
 {
---
+++ js/login.js
@@ -0,0 +1,8 @@
+function showLogIn()
+{
+	
+	console.log("Show log in called");
+	$('#modalBody').html("Patataon");
+	$('#modalTitle').html("Log in");
+	$('#genericModal').modal('show');
+}
